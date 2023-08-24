8d29a535a90db2e112897f0f8a893330393d7f19
JaxTeller <sorin.craciunescu84@gmail.com>
2023-08-24 10:03:54 +0300

    Implemented bookmarks feature and finnished project

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-
+export const API_KEY = 'b6c1678d-90ea-4c56-b912-e4e57ce052b8';
 export const TIMEOUT_SECONDS = 10;
 export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
